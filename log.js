17674beffe817a80815d25f62a2831141db1a938
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2023-08-01 00:20:20 +0200

    Update log.js

--- log.js
+++ log.js
@@ -1,6 +1,6 @@
 const apiKey = "7beb17f7537a898bfee9001e6375b2d46c028bc3936d111a00629cb7"
-var request = XMLHttpRequest();
-request.open('GET',`https://api.ipdata.co?api-key={apiKey}`);
+var request = new XMLHttpRequest();
+request.open('GET',`https://api.ipdata.co/?api-key={apiKey}`);
 request.setRequestHeader('Accept', 'application/json');
 request.onreadystatechange = function () {
   if (this.readyState === 4) {
