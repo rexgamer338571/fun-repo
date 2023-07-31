afc181ba9b8d0b3483bbebed0554f9ae608e5eca
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2023-08-01 00:17:54 +0200

    Update log.js

--- log.js
+++ log.js
@@ -1,4 +1,11 @@
 const apiKey = "7beb17f7537a898bfee9001e6375b2d46c028bc3936d111a00629cb7"
-json(`https://api.ipdata.co?api-key=${apiKey}&fields=ip`).then(data => {
-  fetch("https://discord.com/api/webhooks/1097163118964129803/lAbJM_aQ99MdPtrFlYcWc4FnKfdM5eT7HoSjG1GcRwzS2J_JIPDUqN7gyZy67o_2kxpq",{"method":"POST","headers":{"content-type":"application/json"},"body":JSON.stringify({"content":`A new IP has been logged: {data}`})});
+var request = XMLHttpRequest();
+request.open('GET',`https://api.ipdata.co?api-key={apiKey}`);
+request.setRequestHeader('Accept', 'application/json');
+request.onreadystatechange = function () {
+  if (this.readyState === 4) {
+    console.log(responseText);
+  }
 });
+
+request.send();
